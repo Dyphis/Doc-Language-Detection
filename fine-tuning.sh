748551768f490902803c503bdd98fe5e7c101709
Dyphis <51278619+Dyphis@users.noreply.github.com>
2023-07-26 16:53:32 +0800

    redo dos2unix

---
+++ fine-tuning.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Set the name of the virtual environment folder
+ENV_FOLDER="venv"
+script_name=$0
+script_full_path=$(dirname "$0")
+cd $script_full_path
+
+if [ -d "$ENV_FOLDER" ]; then
+  echo "Virtual environment folder found. Activating..."
+  source $ENV_FOLDER/bin/activate
+  if [ $? -eq 0 ]; then
+    echo "Virtual environment activated."
+    python fine-tuning.py
+    deactivate
+  else
+    echo "Failed to activate virtual environment. Please run initialization first."
+  fi
+else
+  echo "Virtual environment not detected. Please run initialization first."
+fi
+
+exit 0
